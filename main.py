edfd0ceed5ec5d31278703b502e12b236249d3f8
Benny Wang <bennywang75@hotmail.com>
2019-09-30 21:44:01 -0700

    Added comments to functions and modules

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
-import dcsteno
 
+import dcimage
+import dcstego
 
-# print('creating image')
-# dcsteno.steno_image()
+key = b'0123456789abcdef'
+msg = b'The quick brown fox jumps over the lazy dog'
+
+width, height = dcimage.get_image_size('images/jojo_meme_001.png')
+
+print('creating image')
+steno_image = dcstego.steno_image(key, msg, 'images/jojo_meme_001.png')
+dcimage.save_bytes_to_image(steno_image, 'images/jojo_meme_001_stuffed.png', width, height)
+
 print('extracting message')
-dcsteno.unsteno_image()
+secret_msg = dcstego.unsteno_image(key, 'images/jojo_meme_001_stuffed.png')
+print(secret_msg)
